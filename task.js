0646aa3e9e7f6a64642ba7fd71f8606c55ea81e6
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-28 11:47:29 +0200

    add cleanValues function

--- task.js
+++ task.js
@@ -1,12 +1,18 @@
 let allTasks = [];
+let title;
+let date;
+let category;
+let urgency;
+let description;
+let newTask
 
 async function addTask() {
-    let title = document.getElementById('taskTitle');
-    let date = document.getElementById('taskDate');
-    let category = document.getElementById('category');
-    let urgency = document.getElementById('urgency');
-    let description = document.getElementById('taskDescription');
-    let newTask = {
+    title = document.getElementById('taskTitle');
+    date = document.getElementById('taskDate');
+    category = document.getElementById('category');
+    urgency = document.getElementById('urgency');
+    description = document.getElementById('taskDescription');
+    newTask = {
         'title': title.value,
         'date': date.value,
         'category': category.value,
@@ -16,6 +22,19 @@ async function addTask() {
 
     allTasks.push(newTask);
     await backend.setItem('allTasks', JSON.stringify(allTasks));
+    cleanValues();
     console.log(allTasks);
+
+}
+
+function cleanValues() {
+    title.value = '';
+    date.value = '';
+    category.value = '';
+    urgency.value = '';
+    description.value = '';
+    newTask.value = '';
+
+
 
 }
