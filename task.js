a4b610597d9b5d0206948c78fc56c2d1e0163a08
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-28 10:25:13 +0200

    add function addTask

---
+++ task.js
@@ -0,0 +1,21 @@
+let allTasks = [];
+
+async function addTask() {
+    let title = document.getElementById('taskTitle');
+    let date = document.getElementById('taskDate');
+    let category = document.getElementById('category');
+    let urgency = document.getElementById('urgency');
+    let description = document.getElementById('taskDescription');
+    let newTask = {
+        'title': title.value,
+        'date': date.value,
+        'category': category.value,
+        'urgency': urgency.value,
+        'description': description.value
+    };
+
+    allTasks.push(newTask);
+    //  await backend.setItem('allTasks', JSON.stringify(allTasks));
+    console.log(allTasks);
+
+}
